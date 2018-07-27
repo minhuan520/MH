e2ba7a29ee95d127b35896acddf99127c28340b8
minhuan520 <40617168+minhuan520@users.noreply.github.com>
2018-07-27 15:09:16 +0800

    day1:guess num

--- Day1/test01_guessnum.py
+++ Day1/test01_guessnum.py
@@ -1,12 +1,18 @@
 import random
 
-num = int(input('猜下随机生成的数字：'))
 age = random.randint(0, 100)
-while num != age:
-    if num > age:
-        print('比你猜的小')
+print(age)
+num = int(input('猜下随机生成的数字：'))
+n = 1
+while n <= 3:
+    if num == age:
+        print('恭喜你猜对了')
     else:
-        print('比你猜的大')
+        if num > age:
+            print('比你猜的小')
+        else:
+            print('比你猜的大')
     num = int(input('根据提示继续猜：'))
+    n += 1
 else:
-    print('恭喜你猜对了')
+    print('你已猜错3次了，没有机会了')
